bcf440ba8d59e843fc3ea1107f667515bb559102
Praveen Angyan <praveen.angyan@gmail.com>
2017-04-18 21:46:46 -0700

    Initial commit

---
+++ Tests/Tiled/MapTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Tests.Tiled
+{
+    [TestFixture]
+    public class MapTests
+    {
+        [Test]
+        public void Load_WhenGivenAFullPathToATmxFile_LoadsTheTiledMap()
+        {
+            Map map = Map.Load("");
+
+
+        }
+    }
+}
