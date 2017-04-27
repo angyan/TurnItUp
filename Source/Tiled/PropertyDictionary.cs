de92a721ac5bdbdc6675fb5106eee5abbbb03471
Praveen Angyan <angyan@usc.edu>
2017-04-26 17:05:35 -0700

    Add tests for PropertyDictionary

--- Source/Tiled/PropertyDictionary.cs
+++ Source/Tiled/PropertyDictionary.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Turnable.Tiled
 {
     public class PropertyDictionary : Dictionary<string, Property>
     {
+        public PropertyDictionary()
+        {
+        }
+
+        public PropertyDictionary(XElement properties)
+        {
+            foreach(XElement property in properties.Descendants())
+            {
+                Console.WriteLine(property);
+            }
+        }
     }
 }
--- Tests/Tiled/PropertyDictionaryTests.cs
+++ Tests/Tiled/PropertyDictionaryTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
 using Turnable.Tiled;
+using System.Xml.Linq;
+using System.Collections.Generic;
 
 namespace Tests.Tiled
 {
@@ -8,8 +10,21 @@ namespace Tests.Tiled
     public class PropertyDictionaryTests
     {
         [Test]
-        public void TestMethod1()
+        public void ParameterlessConstructor_Exists()
         {
+            var properties = new PropertyDictionary();
+
+            Assert.That(properties.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Constructor_GivenACollectionOfXElements_InitializesAllProperties()
+        {
+            var propertiesXElement = new XElement("properties", new XAttribute("name", "enemyTint"), new XAttribute("type", "color"), new XAttribute("value", "#ffa33636"));
+
+            var properties = new PropertyDictionary(propertiesXElement);
+
+            Assert.That(properties.Count, Is.EqualTo(1));
         }
     }
 }
